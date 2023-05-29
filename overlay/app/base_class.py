8fbdb24d2fa38e2e018fec18783c71dc2b46f422
Gruzin <grzegorz.jagielski.2019@zsa.pwr.edu.pl>
2023-05-30 01:23:14 +0200

    First introduction to redis

--- overlay/app/base_class.py
+++ overlay/app/base_class.py
@@ -1,9 +1,10 @@
-
 import os
 import configparser
+
+import redis
+
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-
 from fastapi import FastAPI, Request, WebSocket, WebSocketDisconnect
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
@@ -22,18 +23,6 @@ class StreamOverlay(FastAPI):
         self._configparser = configparser.ConfigParser()
         self._configparser.read(config_file)
 
-        self.timer = TimerState()
-        self.info_bar = []
-        self.emblem_visible = self._configparser.getboolean('DEFAULT', 'emblem_visible')
-        self.comment_mode = self._configparser.getboolean('DEFAULT', 'comment_mode')
-        self.infobar_visible = self._configparser.getboolean('DEFAULT', 'infobar_visible')
-        self.map_state = {"visible": False,
-                     "team1": "Vape Clan",
-                     "team2": "D0mmyM0mmies"}
-        self.overlay_mode = self._configparser.get("DEFAULT", "overlay_mode")
-
-        self.teams = ["Nieznany", "Nieznany"]
-
         with open("/home/wybory/strings.txt") as file:
             self.predefs = [item.strip() for item in file.read().split("\n")]
 
@@ -44,13 +33,60 @@ class StreamOverlay(FastAPI):
         self.mount("/static", StaticFiles(directory="/home/wybory/static"), name="static")
         self.templating = Jinja2Templates(directory="/home/wybory/templates")
 
+        self.redis: redis.Redis
+        self._setup_redis()
+        self._update_values()
+
+    def _setup_redis(self):
+        self.redis = redis.Redis(host=os.getenv("BROKER_BACKEND_URL"), db=0)
+
+    def _update_values(self):
+        """
+        Ensures that all values on redis are set
+        :return:
+        """
+        self.timer = TimerState()
+
+        # info_bar = self.redis.llen("info_bar")
+        # if info_bar == 0:
+        #     self.redis.lpush("info_bar", *[])
+
+        emblem_visible = self.redis.get("emblem_visible")
+        if emblem_visible is None:
+            self.redis.set("emblem_visible", True)
+
+        comment_mode = self.redis.get("comment_mode")
+        if comment_mode is None:
+            self.comment_mode = False
+            
+        infobar_visible = self.redis.get("infobar_visible")
+        if infobar_visible is None:
+            self.redis.set("infobar_visible", True)
+
+        map_state = self.redis.hgetall("map_state")
+        if not map_state:
+            self.redis.hset("map_state", {"visible": False,
+                                          "team1": "Vape Clan",
+                                          "team2": "D0mmyM0mmies"})
+
+        overlay_mode = self.redis.get("overlay_mode")
+        if overlay_mode is None:
+            self.redis.set("overlay_mode", "GP")
+
+        teams = self.redis.llen("teams")
+        if teams == 0:
+            self.redis.lpush("teams", *["NA", "NA"])
+
     def dump_to_config(self):
-        self._configparser.set('DEFAULT', 'emblem_visible', str(self.emblem_visible))
-        self._configparser.set('DEFAULT', 'comment_mode', str(self.comment_mode))
-        self._configparser.set('DEFAULT', 'infobar_visible', str(self.infobar_visible))
-        self._configparser.set("DEFAULT", "overlay_mode", str(self.overlay_mode))
-        with open(self._config_file, 'w') as file:
+        with open(self._config_file, 'w+') as file:
             self._configparser.write(file)
+
+    def redis_to_cfg(self):
+        self._configparser.set("DEFAULT", "emblem_visible", str(self.redis.get("emblem_visible")))
+        self._configparser.set("DEFAULT", "comment_mode", str(self.redis.get("comment_mode")))
+        self._configparser.set("DEFAULT", "infobar_visible", str(self.redis.get("infobar_visible")))
+        self._configparser.set("DEFAULT", "overlay_mode", str(self.redis.get("overlay_mode")))
+        self.dump_to_config()
 
     @staticmethod
     def populate_deafult_config(config_file):
